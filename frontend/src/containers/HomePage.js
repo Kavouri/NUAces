a4977cccdbddcd98774fe93bb049166098a00e9c
tdcskate100 <carroll.t@husky.neu.edu>
2017-10-25 14:36:53 -0400

    Update HomePage.js

--- frontend/src/containers/HomePage.js
+++ frontend/src/containers/HomePage.js
@@ -3,11 +3,7 @@ import HomePageView from '../views/HomePageView';
 
 const mapStateToProps = (state) => {
   return {
-<<<<<<< HEAD
     partners: state.partners.partnerList,
-=======
-    partners: state.partners,
->>>>>>> b36afb98ca1ed5d3c3352ddb84f7e52df4539fc7
   };
 };
 
