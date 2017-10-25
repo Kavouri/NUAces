13ed66b6c3924b2ff1b6b8b1f4028f50053d2968
tdcskate100 <carroll.t@husky.neu.edu>
2017-10-25 14:38:21 -0400

    Update reducers.js

--- frontend/src/redux/reducers/reducers.js
+++ frontend/src/redux/reducers/reducers.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import { combineReducers } from 'redux';
-<<<<<<< HEAD
 import { ADD_EVENT, REGISTER_FOR_EVENT } from '../actions/actions';
 import { REQUEST_PARTNERS, RECEIVE_PARTNERS } from '../actions/partnerActions';
 
@@ -20,19 +19,6 @@ function partners(state = { isFetchingPartners: false, partnerList: [], received
         partnerList: action.partners,
         receivedAt: action.receivedAt,
       };
-=======
-import { ADD_PARTNER, ADD_EVENT, REGISTER_FOR_EVENT } from '../actions/actions';
-
-// Reducer for adding partners.
-// We still need to add the async post.
-function partners(state = [], action) {
-  switch (action.type) {
-    case ADD_PARTNER:
-      return [
-        ...state,
-        action.partnerInfo,
-      ];
->>>>>>> b36afb98ca1ed5d3c3352ddb84f7e52df4539fc7
     default:
       return state;
   }
